fff2b2f858765aae045c126e0e68976397fd7a27
ratzghacct <rathish.inbox@gmail.com>
2025-04-18 14:06:56 +0530

    Delete hooks/toast.ts

--- hooks/toast.ts
+++
@@ -1,9 +0,0 @@
-import { useCallback } from 'react';
-
-export function useToast() {
-  const toast = useCallback(({ title, description }: { title: string; description: string }) => {
-    alert(`${title}\n\n${description}`);
-  }, []);
-
-  return { toast };
-}
